8e49bd61e9b9e907705628872d68c7d08e886a08
OStefani <ostefanishina@gmail.com>
2019-05-19 20:36:51 +0300

    Build

---
+++ build/precache-manifest.fb5d8de1eab5e3618734f8bbdebc0114.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1286083edfd2faf5961194649ac8ee14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43fabd13e2363fe1e879",
+    "url": "/static/css/main.f078c7b5.chunk.css"
+  },
+  {
+    "revision": "1992c6e4ffe2a3f31cfd",
+    "url": "/static/js/2.c8cc3194.chunk.js"
+  },
+  {
+    "revision": "43fabd13e2363fe1e879",
+    "url": "/static/js/main.af6eb8ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
